a70c7cc64aed9ffc6a65649338af4e7bbf8c7c38
Zoltan Sebestyen <63124624+sebizoli@users.noreply.github.com>
2020-11-25 11:49:50 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+function btncons() {
+  document.getElementById("consh").style.display = "block";
+  document.getElementById("skillh").style.display = "none";
+  document.getElementById("devh").style.display = "none";
+  document.getElementById("analh").style.display = "none";
+  document.getElementById("consb").style.display = "block";
+  document.getElementById("skillb").style.display = "none";
+  document.getElementById("devb").style.display = "none";
+  document.getElementById("analb").style.display = "none";
+}
+
+function btnskill() {
+  document.getElementById("consh").style.display = "none";
+  document.getElementById("skillh").style.display = "block";
+  document.getElementById("devh").style.display = "none";
+  document.getElementById("analh").style.display = "none";
+  document.getElementById("consb").style.display = "none";
+  document.getElementById("skillb").style.display = "block";
+  document.getElementById("devb").style.display = "none";
+  document.getElementById("analb").style.display = "none";
+}
+
+function btndev() {
+  document.getElementById("consh").style.display = "none";
+  document.getElementById("skillh").style.display = "none";
+  document.getElementById("devh").style.display = "block";
+  document.getElementById("analh").style.display = "none";
+  document.getElementById("consb").style.display = "none";
+  document.getElementById("skillb").style.display = "none";
+  document.getElementById("devb").style.display = "block";
+  document.getElementById("analb").style.display = "none";
+}
+
+function btnanal() {
+  document.getElementById("consh").style.display = "none";
+  document.getElementById("skillh").style.display = "none";
+  document.getElementById("devh").style.display = "none";
+  document.getElementById("analh").style.display = "block";
+  document.getElementById("consb").style.display = "none";
+  document.getElementById("skillb").style.display = "none";
+  document.getElementById("devb").style.display = "none";
+  document.getElementById("analb").style.display = "block";
+}
